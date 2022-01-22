c9bb1518c3fbbcdbecc1e6007e2e2e4506e0b8fa
Namratha <58727290+Namratha-shivaraju@users.noreply.github.com>
2022-01-22 18:44:54 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,7 @@
-/**
-* Template Name: Kelly - v4.7.0
-* Template URL: https://bootstrapmade.com/kelly-free-bootstrap-cv-resume-html-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
-  /**
-   * Easy selector helper function
-   */
   const select = (el, all = false) => {
     el = el.trim()
     if (all) {
@@ -19,9 +11,7 @@
     }
   }
 
-  /**
-   * Easy event listener function
-   */
+
   const on = (type, el, listener, all = false) => {
     let selectEl = select(el, all)
     if (selectEl) {
@@ -33,16 +23,12 @@
     }
   }
 
-  /**
-   * Easy on scroll event listener 
-   */
+ 
   const onscroll = (el, listener) => {
     el.addEventListener('scroll', listener)
   }
 
-  /**
-   * Scrolls to an element with header offset
-   */
+  
   const scrollto = (el) => {
     let header = select('#header')
     let offset = header.offsetHeight
@@ -54,9 +40,7 @@
     })
   }
 
-  /**
-   * Back to top button
-   */
+  
   let backtotop = select('.back-to-top')
   if (backtotop) {
     const toggleBacktotop = () => {
@@ -70,18 +54,14 @@
     onscroll(document, toggleBacktotop)
   }
 
-  /**
-   * Mobile nav toggle
-   */
+ 
   on('click', '.mobile-nav-toggle', function(e) {
     select('#navbar').classList.toggle('navbar-mobile')
     this.classList.toggle('bi-list')
     this.classList.toggle('bi-x')
   })
 
-  /**
-   * Mobile nav dropdowns activate
-   */
+ 
   on('click', '.navbar .dropdown > a', function(e) {
     if (select('#navbar').classList.contains('navbar-mobile')) {
       e.preventDefault()
@@ -89,9 +69,7 @@
     }
   }, true)
 
-  /**
-   * Scrool with ofset on links with a class name .scrollto
-   */
+ 
   on('click', '.scrollto', function(e) {
     if (select(this.hash)) {
       e.preventDefault()
@@ -107,9 +85,6 @@
     }
   }, true)
 
-  /**
-   * Scroll with ofset on page load with hash links in the url
-   */
   window.addEventListener('load', () => {
     if (window.location.hash) {
       if (select(window.location.hash)) {
@@ -118,9 +93,7 @@
     }
   });
 
-  /**
-   * Preloader
-   */
+  
   let preloader = select('#preloader');
   if (preloader) {
     window.addEventListener('load', () => {
@@ -128,9 +101,7 @@
     });
   }
 
-  /**
-   * Porfolio isotope and filter
-   */
+  
   window.addEventListener('load', () => {
     let portfolioContainer = select('.portfolio-container');
     if (portfolioContainer) {
@@ -158,30 +129,24 @@
 
   });
 
-  /**
-   * Initiate portfolio lightbox 
-   */
+  
   const portfolioLightbox = GLightbox({
     selector: '.portfolio-lightbox'
   });
 
-  /**
-   * Initiate portfolio details lightbox 
-   */
+  
   const portfolioDetailsLightbox = GLightbox({
     selector: '.portfolio-details-lightbox',
     width: '90%',
     height: '90vh'
   });
 
-  /**
-   * Portfolio details slider
-   */
+  
   new Swiper('.portfolio-details-slider', {
     speed: 400,
     loop: true,
     autoplay: {
-      delay: 9000,
+      delay: 5000,
       disableOnInteraction: false
     },
     pagination: {
@@ -191,9 +156,38 @@
     }
   });
 
-  /**
-   * Animation on scroll
-   */
+  
+  let skilsContent = select('.skills-content');
+  if (skilsContent) {
+    new Waypoint({
+      element: skilsContent,
+      offset: '80%',
+      handler: function(direction) {
+        let progress = select('.progress .progress-bar', true);
+        progress.forEach((el) => {
+          el.style.width = el.getAttribute('aria-valuenow') + '%'
+        });
+      }
+    })
+  }
+
+ 
+  new Swiper('.testimonials-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+  
   window.addEventListener('load', () => {
     AOS.init({
       duration: 1000,
